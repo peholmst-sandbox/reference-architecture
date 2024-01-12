e40b2d68c0448e1b35dffa7bafb3acf51fc11d19
Petter Holmström <petter@vaadin.com>
2024-01-12 17:24:37 +0200

    Remove unneeded exports.

--- shallow-anemic-domain-model/single-module-project/src/main/java/com/example/demo/services/ContactService.java
+++ shallow-anemic-domain-model/single-module-project/src/main/java/com/example/demo/services/ContactService.java
@@ -1,8 +1,6 @@
 package com.example.demo.services;
 
 import com.example.demo.domain.Contact;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
